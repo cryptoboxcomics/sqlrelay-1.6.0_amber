bda2d52edeb6afb25744b22e2841b105b1e4cd01
Amber Lee <Amber.Lee@ses.com>
2019-10-22 16:00:30 -0400

    modified compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f /tmp/recentqueries.log
-rm -f /tmp/debugger.txt
-/usr/local/firstworks/bin/sqlr-stop -id mssql2test
 ./configure; make; make install
+cp sqlrelay_frequency.service /etc/systemd/system
+cp deleterecent.sh /usr/local/firstworks
+systemctl enable sqlrelay_frequency.service
+systemctl start sqlrelay_frequency.service
